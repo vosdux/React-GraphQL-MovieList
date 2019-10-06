1232dad0cd5f945a6a280db8b03cb7753c8efdda
Artem <karpuxov.a@mail.ru>
2019-10-06 22:38:54 +0300

    Добавление режисера

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -1,6 +1,17 @@
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
+
+import { addDirectorMutation } from './mutations';
 
 import { styles } from './styles';
 
-export default compose(withStyles(styles));
+const withGraphqlAdd = graphql(addDirectorMutation, {
+    props: ({ mutate }) => ({
+        addDirector: director => mutate({
+            variables: director
+        })
+    })
+})
+
+export default compose(withStyles(styles), withGraphqlAdd);
